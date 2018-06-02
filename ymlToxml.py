019cc7512c4f0a27f94df45242d99820f2434bcc
msimiste <mikesimister@shaw.ca>
2018-06-01 23:30:01 -0600

    Able to parse yaml and write to custom xml file. It's not pretty but mostly working

--- ymlToxml.py
+++ ymlToxml.py
@@ -3,11 +3,19 @@
 import sys
 import json
 import yaml
-import xml.etree.cElementTree
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
 import dicttoxml
 import collections
 
 
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+    
 
 def recursive_items(dictionary):
     for key, value in dictionary.items():
@@ -38,7 +46,18 @@ def outputToXML(data):
     ff = open('output.xml','w+')
     ff.write(data)
     ff.close()
-              
+    
+def customXML(data):
+    ff = ff = open('output.xml','w+')
+    root = ET.Element("root")
+    sub1 = ET.SubElement(root,"value")
+    for i,v in enumerate(data):
+        ET.SubElement(root,"value", name=v).text = data.get(v).encode('utf-8').decode('utf-8')
+        
+    tree = ET.ElementTree(root)
+    tree.write('output.xml', xml_declaration=True, encoding="UTF-8")
+    ff.close()
+    
 def main():
     f = open('en.yml', 'rb')
     ymlData = yaml.load(f)
@@ -49,7 +68,8 @@ def main():
     #print(ymlData)
     #print(xmlData)
     #print(ymlData.values())
-    outputToXML(xmlData)
+    #outputToXML(xmlData)
+    customXML(ymlData)
     
     
         #print(ymlData[v])
